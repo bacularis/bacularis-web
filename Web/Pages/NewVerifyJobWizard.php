dc78ef45d5c01fa737b3cba3afc7b2b4bc6deaf7
Marcin Haba <marcin.haba@bacula.pl>
2022-08-10 04:41:06 +0200

    Add missing license statement

--- Web/Pages/NewVerifyJobWizard.php
+++ Web/Pages/NewVerifyJobWizard.php
@@ -6,6 +6,11 @@
  *
  * The main author of Bacularis is Marcin Haba, with contributors, whose
  * full list can be found in the AUTHORS file.
+ *
+ * You may use this file and others of this release according to the
+ * license defined in the LICENSE file, which includes the Affero General
+ * Public License, v3.0 ("AGPLv3") and some additional permissions and
+ * terms pursuant to its AGPLv3 Section 7.
  */
 
 use Bacularis\Web\Modules\BaculumWebPage;
