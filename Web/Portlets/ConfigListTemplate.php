04a44a3b2625d7615ea5e4236045e660638184d9
Marcin Haba <marcin.haba@bacula.pl>
2024-01-07 03:13:46 +0100

    Update year

--- Web/Portlets/ConfigListTemplate.php
+++ Web/Portlets/ConfigListTemplate.php
@@ -2,7 +2,7 @@
 /*
  * Bacularis - Bacula web interface
  *
- * Copyright (C) 2021-2023 Marcin Haba
+ * Copyright (C) 2021-2024 Marcin Haba
  *
  * The main author of Bacularis is Marcin Haba, with contributors, whose
  * full list can be found in the AUTHORS file.
