7721685401f25adf0f0a253c16a494a3a75f8f2b
Marcin Haba <marcin.haba@bacula.pl>
2024-10-09 13:35:15 +0200

    Add new cloud storage wizard

---
+++ Web/Modules/StorageTools.php
@@ -0,0 +1,259 @@
+<?php
+/*
+ * Bacularis - Bacula web interface
+ *
+ * Copyright (C) 2021-2024 Marcin Haba
+ *
+ * The main author of Bacularis is Marcin Haba, with contributors, whose
+ * full list can be found in the AUTHORS file.
+ *
+ * You may use this file and others of this release according to the
+ * license defined in the LICENSE file, which includes the Affero General
+ * Public License, v3.0 ("AGPLv3") and some additional permissions and
+ * terms pursuant to its AGPLv3 Section 7.
+ */
+
+namespace Bacularis\Web\Modules;
+
+/**
+ * Module responsible for providing various storage tools.
+ *
+ * @author Marcin Haba <marcin.haba@bacula.pl>
+ * @category Module
+ */
+class StorageTools extends WebModule
+{
+	/**
+	 * Default storage daemon port.
+	 */
+	private const DEFAULT_SD_PORT = 9103;
+
+	/**
+	 * Get the storage daemon password from the SD config.
+	 *
+	 * @param string $api_host API host
+	 * @param string $director director name
+	 * return string SD password or empty string is password was not possible to get
+	 */
+	public function getSdPassword(string $api_host, string $director): string
+	{
+		$api = $this->getModule('api');
+		$result = $api->get(
+			['config', 'sd', 'Director', $director],
+			$api_host,
+			false
+		);
+		$sd_password = '';
+		if ($result->error == 0) {
+			$sd_password = $result->output->Password;
+		}
+		return $sd_password;
+	}
+
+	/**
+	 * Get the storage daemon port from the SD config.
+	 *
+	 * @param string $api_host API host
+	 * @return int storage daemon port
+	 */
+	public function getSdPort(string $api_host): int
+	{
+		$api = $this->getModule('api');
+		$result = $api->get(
+			['config', 'sd', 'Storage'],
+			$api_host,
+			false
+		);
+		$sd_port = 0;
+		if ($result->error == 0) {
+			$sd_port = (int) ($result->output[0]->Storage->SdPort ?? self::DEFAULT_SD_PORT);
+		}
+		return $sd_port;
+	}
+
+	/**
+	 * Create storage device.
+	 *
+	 * @param string $api_host API host
+	 * @param array $directives directives
+	 * @param array $def_directives default directives
+	 * @return object with output and error code
+	 */
+	public function createDevice(string $api_host, array $directives, array $def_directives = []): object
+	{
+		$api = $this->getModule('api');
+		$params = [
+			'config',
+			'sd',
+			'Device',
+			$directives['Name']
+		];
+		$config = $directives;
+		if (count($def_directives) > 0) {
+			$config = array_merge($def_directives, $config);
+		}
+		$result = $api->create(
+			$params,
+			['config' => json_encode($config)],
+			$api_host,
+			false
+		);
+		return $result;
+	}
+
+	/**
+	 * Create storage autochanger.
+	 *
+	 * @param string $api_host API host
+	 * @param array $directives directives
+	 * @param array $def_directives default directives
+	 * @return object with output and error code
+	 */
+	public function createAutochanger(string $api_host, array $directives, array $def_directives = []): object
+	{
+		$api = $this->getModule('api');
+		$params = [
+			'config',
+			'sd',
+			'Autochanger',
+			$directives['Name']
+		];
+		$config = $directives;
+		if (count($def_directives) > 0) {
+			$config = array_merge($def_directives, $config);
+		}
+		$result = $api->create(
+			$params,
+			['config' => json_encode($config)],
+			$api_host,
+			false
+		);
+		return $result;
+	}
+
+	/**
+	 * Create storage resource in current API host.
+	 *
+	 * @param array $directives directives
+	 * @param array $def_directives default directives
+	 * @return object with output and error code
+	 */
+	public function createStorage(array $directives, array $def_directives = []): object
+	{
+		$api = $this->getModule('api');
+		$params = [
+			'config',
+			'dir',
+			'Storage',
+			$directives['Name']
+		];
+		$config = $directives;
+		if (count($def_directives) > 0) {
+			$config = array_merge($def_directives, $config);
+		}
+		$result = $api->create(
+			$params,
+			['config' => json_encode($config)],
+			null,
+			false
+		);
+		return $result;
+	}
+
+	/**
+	 * Create storage cloud resource.
+	 *
+	 * @param string $api_host API host
+	 * @param array $directives directives
+	 * @return object with output and error code
+	 */
+	public function createCloud(string $api_host, array $directives): object
+	{
+		$api = $this->getModule('api');
+		$params = [
+			'config',
+			'sd',
+			'Cloud',
+			$directives['Name']
+		];
+		$config = $directives;
+		$result = $api->create(
+			$params,
+			['config' => json_encode($config)],
+			$api_host,
+			false
+		);
+		return $result;
+	}
+
+	/**
+	 * Get storage catalog identifier by storage address.
+	 *
+	 * @param string $sd_address storage daemon address
+	 * @return int storage identifier or 0 if the intentifier could not be found
+	 */
+	public function getStorageIdByAddress(string $sd_address): int
+	{
+		$api = $this->getModule('api');
+		$params = [
+			'config',
+			'dir',
+			'Storage'
+		];
+		$storage_name = '';
+		$result = $api->get($params);
+		if ($result->error == 0) {
+			for ($i = 0; $i < count($result->output); $i++) {
+				$addr = $result->output[$i]->Storage->Address ?? '';
+				if ($addr === $sd_address) {
+					$storage_name = $result->output[$i]->Storage->Name;
+					break;
+				}
+			}
+		}
+		$storage_id = 0;
+		if ($storage_name) {
+			$query_params = [
+				'name' => $storage_name
+			];
+			$query = '?' . http_build_query($query_params);
+			$params = [
+				'storages',
+				$query
+			];
+			$result = $api->get($params);
+			if ($result->error == 0) {
+				$storage_id = $result->output[0]->storageid ?? 0;
+			}
+		}
+		return $storage_id;
+	}
+
+	/**
+	 * Get running job number on the given storage.
+	 *
+	 * @param int $storage_id storage identifier
+	 * @return int running job number
+	 */
+	public function getRunningJobNumber(int $storage_id): int
+	{
+		$api = $this->getModule('api');
+		$query_params = [
+			'output' => 'json',
+			'type' => 'header'
+		];
+		$query = '?' . http_build_query($query_params);
+		$params = [
+			'storages',
+			$storage_id,
+			'status',
+			$query
+		];
+		$running_job_nb = 0;
+		$result = $api->get($params);
+		if ($result->error == 0) {
+			$running_job_nb = $result->output->jobs_running ?? 0;
+		}
+		return $running_job_nb;
+	}
+}
---
+++ Web/Pages/NewCloudStorageWizard.php
@@ -0,0 +1,400 @@
+<?php
+/*
+ * Bacularis - Bacula web interface
+ *
+ * Copyright (C) 2021-2024 Marcin Haba
+ *
+ * The main author of Bacularis is Marcin Haba, with contributors, whose
+ * full list can be found in the AUTHORS file.
+ *
+ * You may use this file and others of this release according to the
+ * license defined in the LICENSE file, which includes the Affero General
+ * Public License, v3.0 ("AGPLv3") and some additional permissions and
+ * terms pursuant to its AGPLv3 Section 7.
+ */
+
+use Bacularis\Web\Modules\BaculumWebPage;
+use Prado\Prado;
+
+class NewCloudStorageWizard extends BaculumWebPage
+{
+	public $storage_created;
+	public $storage_address;
+	public $storage_create_errors;
+	public $storage_running_jobs_no = 0;
+
+	private const STORAGE_TYPE_SINGLE = 'Single';
+	private const STORAGE_TYPE_AUTOCHANGER = 'Autochanger';
+
+	private const DEF_DEVICE_DIRECTIVES = [
+		'DeviceType' => 'Cloud',
+		'RemovableMedia' => false,
+		'AutomaticMount' => true,
+		'LabelMedia' => true,
+		'MaximumConcurrentJobs' => 1,
+		'RandomAccess' => true,
+		'MaximumPartSize' => 5368709120, // 5 GiB
+		'MaximumFileSize' => 5368709120 // 5 GiB
+	];
+
+	private const DEF_AUTOCHANGER_DIRECTIVES = [
+		'ChangerCommand' => '/dev/null',
+		'ChangerDevice' => '/dev/null'
+	];
+
+	private const DEF_SINGLE_STORAGE_DIRECTIVES = [
+		'MaximumConcurrentJobs' => 1
+	];
+
+	private const DEF_AUTOCHANGER_STORAGE_DIRECTIVES = [
+		'MaximumConcurrentJobs' => 50
+	];
+
+	public const PREV_STEP = 'PrevStep';
+
+	public function wizardStop($sender, $param)
+	{
+		$this->goToPage('StorageList');
+	}
+
+	public function onPreRender($param)
+	{
+		parent::onPreRender($param);
+		if ($this->IsCallBack) {
+			return;
+		}
+		$step_index = $this->NewStorageWizard->getActiveStepIndex();
+		$prev_step = $this->getPrevStep();
+		$this->setPrevStep($step_index);
+		if ($prev_step > $step_index) {
+			return;
+		}
+		switch ($step_index) {
+			case 0:	{
+				$this->loadAPIHosts();
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Load API hosts (step 1).
+	 *
+	 */
+	public function loadAPIHosts()
+	{
+		$api_hosts = $this->User->getAPIHosts();
+		array_unshift($api_hosts, '');
+
+		$this->StorageAPIHost->DataSource = array_combine($api_hosts, $api_hosts);
+		$this->StorageAPIHost->dataBind();
+	}
+
+	/**
+	 * Check if selected API host have the SD with a cloud plugin.
+	 *
+	 * @param TActiveDropDownList $sender sender object
+	 * @param TCallbackEventParameter $param callback parameter
+	 */
+	public function checkCloudPlugins($sender, $param)
+	{
+		$err_id = 'api_host_storage_error';
+		$cb = $this->getCallbackClient();
+		$cb->hide($err_id);
+		$cb->update('api_host_storage_daemon_name', '-');
+
+		$api_host = $this->StorageAPIHost->getSelectedValue();
+		$api_hosts = $this->User->getAPIHosts();
+		if (!in_array($api_host, $api_hosts)) {
+			// unauthorized API host, return...
+			return false;
+		}
+		$result = $this->getModule('api')->get(
+			['config'],
+			$api_host
+		);
+		$sd_state = false;
+		$sd_name = '';
+		$sd_found = false;
+		$sd_error = '';
+		if ($result->error == 0) {
+			for ($i = 0; $i < count($result->output); $i++) {
+				if ($result->output[$i]->component_type != 'sd') {
+					continue;
+				}
+				$sd_found = true;
+				$sd_name = $result->output[$i]->component_name;
+				$sd_state = $result->output[$i]->state;
+				$sd_error = $result->output[$i]->error_msg;
+				break;
+			}
+
+			if ($sd_found === true) {
+				if ($sd_state === true) {
+					$cb->update(
+						'api_host_storage_daemon_name',
+						$sd_name
+					);
+				} else {
+					$message = Prado::localize('The storage daemon configuration capability on API host \'%s\' is configured but not work correctly. Please check the bsdjson configuration on that API host. Error: %s.');
+					$emsg = sprintf($message, $api_host, $sd_error);
+					$cb->update($err_id, $emsg);
+					$cb->show($err_id);
+				}
+			} else {
+				$message = Prado::localize('The storage daemon configuration capability is not configured on the API host \'%s\'. Please configure the bsdjson on that API host.');
+				$emsg = sprintf($message, $api_host);
+				$cb->update($err_id, $emsg);
+				$cb->show($err_id);
+			}
+		} else {
+			$emsg = sprintf(
+				'Error %d: %s',
+				$result->error,
+				$result->output
+			);
+			$cb->update($err_id, $emsg);
+			$cb->show($err_id);
+		}
+	}
+
+	public function wizardCompleted($sender, $param)
+	{
+		$storage_name = $this->StorageName->getDirectiveValue();
+		$storage_desc = $this->StorageDescription->getDirectiveValue() ?? '';
+		$api_host = $this->StorageAPIHost->getSelectedValue();
+		$api_hosts = $this->User->getAPIHosts();
+		$storage_type = null;
+		$backup_restore_dev_no = 0;
+		$restore_dev_no = 0;
+		if ($this->StorageTypeSingle->Checked) {
+			$storage_type = self::STORAGE_TYPE_SINGLE;
+			$backup_restore_dev_no = 1;
+		} elseif ($this->StorageTypeAutochanger->Checked) {
+			$storage_type = self::STORAGE_TYPE_AUTOCHANGER;
+			$backup_restore_dev_no = (int) $this->StorageNumberOfBackupRestoreDevices->Text;
+			$restore_dev_no = (int) $this->StorageNumberOfRestoreDevices->Text;
+		}
+		$data_vol_dir = $this->StorageDataVolumeDir->Text;
+		$media_type = $this->StorageMediaType->Text;
+
+		if (!in_array($api_host, $api_hosts)) {
+			// unauthorized API host, return...
+			return false;
+		}
+
+		// Get Cloud resource parameters
+		$cloud_name = $storage_name . '-cloud';
+		$cloud_driver = $this->StorageCloudDriver->getSelectedValue();
+		$cloud_hostname = $this->StorageCloudHostName->Text;
+		$cloud_bucket = $this->StorageCloudBucket->Text;
+		$cloud_access_key = $this->StorageCloudAccessKey->Text;
+		$cloud_secret_key = $this->StorageCloudSecretKey->Text;
+		$cloud_region = $this->StorageCloudRegion->Text;
+		$cloud_upload_manual = $this->StorageCloudUploadManual->Checked;
+		$cloud_upload_eachpart = $this->StorageCloudUploadEachPart->Checked;
+		$cloud_upload_atendofjob = $this->StorageCloudUploadAtEndOfJob->Checked;
+		$cloud_protocol = $this->StorageCloudProtocol->getSelectedValue();
+		$cloud_uristyle = $this->StorageCloudUriStyle->getSelectedValue();
+		$cloud_truncate_cache = $this->StorageCloudTruncateCache->getSelectedValue();
+		$cloud_blob_endpoint = $this->StorageCloudBlobEndpoint->Text;
+		$upload = '';
+		if ($cloud_upload_manual) {
+			$upload = 'Manual';
+		} elseif ($cloud_upload_eachpart) {
+			$upload = 'EachPart';
+		} elseif ($cloud_upload_atendofjob) {
+			$upload = 'AtEndOfJob';
+		}
+		$cloud_directives = [
+			'Name' => $cloud_name,
+			'Driver' => $cloud_driver,
+			'HostName' => $cloud_hostname,
+			'BucketName' => $cloud_bucket,
+			'AccessKey' => $cloud_access_key,
+			'SecretKey' => $cloud_secret_key,
+			'Upload' => $upload,
+			'Protocol' => $cloud_protocol,
+			'UriStyle' => $cloud_uristyle,
+			'TruncateCache' => $cloud_truncate_cache
+		];
+		if (!empty($cloud_region)) {
+			$cloud_directives['Region'] = $cloud_region;
+		}
+		if (!empty($cloud_blob_endpoint)) {
+			$cloud_directives['BlobEndpoint'] = $cloud_blob_endpoint;
+		}
+
+		$storage_tools = $this->getModule('storage_tools');
+		$result = $storage_tools->createCloud($api_host, $cloud_directives);
+		if ($result->error != 0) {
+			$this->storage_created = false;
+			$this->storage_create_errors = $result->output;
+			return;
+		}
+
+		// Get SD password
+		$sd_password = $storage_tools->getSdPassword($api_host, $_SESSION['dir']);
+
+		// Get SD port
+		$sd_port = $storage_tools->getSdPort($api_host);
+
+		$host_config = $this->getModule('host_config');
+		$host_params = $host_config->getHostConfig($api_host);
+		$sd_address = $this->storage_address = $host_params['address'] ?? '';
+		if ($sd_address) {
+			$storage_id = $storage_tools->getStorageIdByAddress($sd_address);
+			if ($storage_id > 0) {
+				$this->storage_running_jobs_no = $storage_tools->getRunningJobNumber($storage_id);
+			}
+		}
+
+		$output = '';
+		$error = 0;
+		$errors = [];
+		$devices = [];
+		$total_dev_no = $backup_restore_dev_no + $restore_dev_no;
+		for ($i = 0; $i < $total_dev_no; $i++) {
+			$name = $storage_name;
+			if ($backup_restore_dev_no > 1) {
+				$name = sprintf(
+					'%s-%03d',
+					$storage_name,
+					($i + 1)
+				);
+			}
+			$ro = (($i + 1) > $backup_restore_dev_no);
+			$autochanger = ($storage_type == self::STORAGE_TYPE_AUTOCHANGER);
+			$dev_directives = [
+				'Name' => $name,
+				'Description' => $storage_desc,
+				'DriveIndex' => $i,
+				'Cloud' => $cloud_name,
+				'ArchiveDevice' => $data_vol_dir,
+				'MediaType' => $media_type,
+				'ReadOnly' => $ro,
+				'Autochanger' => $autochanger
+			];
+			$result = $storage_tools->createDevice(
+				$api_host,
+				$dev_directives,
+				self::DEF_DEVICE_DIRECTIVES
+			);
+			$output = $result->output;
+			$error = $result->error;
+			if ($error !== 0) {
+				$errors[] = "Error: {$error}: $output";
+				break;
+			}
+			$devices[] = $name;
+		}
+		if ($error == 0) {
+			$autochanger = false;
+			if ($storage_type == self::STORAGE_TYPE_AUTOCHANGER) {
+				// create autochanger
+				$autochanger = true;
+				$ach_directives = [
+					'Name' => $storage_name,
+					'Device' => $devices
+				];
+				$result = $storage_tools->createAutochanger(
+					$api_host,
+					$ach_directives,
+					self::DEF_AUTOCHANGER_DIRECTIVES
+				);
+				$output = $result->output;
+				$error = $result->error;
+			}
+			if ($error == 0) {
+				// create storage
+				$stor_directives = [
+					'Name' => $storage_name,
+					'Description' => $storage_desc,
+					'Address' => $sd_address,
+					'SdPort' => $sd_port,
+					'Password' => $sd_password,
+					'Device' => $storage_name,
+					'MediaType' => $media_type
+				];
+				$def_directives = [];
+				if ($autochanger) {
+					$stor_directives['Autochanger'] = $autochanger;
+					$def_directives = self::DEF_AUTOCHANGER_STORAGE_DIRECTIVES;
+				} else {
+					$def_directives = self::DEF_SINGLE_STORAGE_DIRECTIVES;
+				}
+				$result = $storage_tools->createStorage($stor_directives, $def_directives);
+				$output = $result->output;
+				$error = $result->error;
+				if ($error != 0) {
+					$output = str_replace([PHP_EOL, '"'], ['<br />', '\"'], $output);
+					$errors[] = "Error: {$error}: $output";
+				}
+			} else {
+				$output = str_replace([PHP_EOL, '"'], ['<br />', '\"'], $output);
+				$errors[] = "Error: {$error}: $output";
+			}
+		}
+		if ($error == 0) {
+			$api = $this->getModule('api');
+			$api->set(['console'], ['reload']);
+			$this->storage_created = true;
+		} else {
+			$this->storage_created = false;
+			$this->storage_create_errors = implode('<br /><br />', $errors);
+		}
+	}
+
+	public function restartStorage($sender, $param)
+	{
+		$api_host = $this->StorageAPIHost->getSelectedValue();
+		$api_hosts = $this->User->getAPIHosts();
+		if (!in_array($api_host, $api_hosts)) {
+			// unauthorized API host, return...
+			return false;
+		}
+		$api = $this->getModule('api');
+		$params = [
+			'actions',
+			'storage',
+			'restart'
+		];
+		$result = $api->get(
+			$params,
+			$api_host
+		);
+		$cb = $this->getCallbackClient();
+		if ($result->error === 0) {
+			$cb->callClientFunction(
+				'oNewCloudStorageStep4.set_restart_result',
+				[true, $result->error, Prado::localize('Done!')]
+			);
+		} else {
+			$cb->callClientFunction(
+				'oNewCloudStorageStep4.set_restart_result',
+				[false, $result->error, $result->output]
+			);
+		}
+	}
+
+	/**
+	 * Set previous wizard step.
+	 *
+	 * @param int $step previous step number
+	 */
+	public function setPrevStep($step)
+	{
+		$step = (int) $step;
+		$this->setViewState(self::PREV_STEP, $step);
+	}
+
+	/**
+	 * Get previous wizard step.
+	 *
+	 * @return int previous wizard step
+	 */
+	public function getPrevStep()
+	{
+		return $this->getViewState(self::PREV_STEP);
+	}
+}
