f4fde5f0a7e2c77f3b3207c8491a97cc1ec878f1
Marcin Haba <marcin.haba@bacula.pl>
2025-07-22 13:28:30 +0200

    Add Debian 13 Trixie support

--- Web/Modules/OSProfileConfig.php
+++ Web/Modules/OSProfileConfig.php
@@ -432,6 +432,7 @@ class OSProfileConfig extends ConfigFileModule
 		'deb' => [
 			'Debian 11 Bullseye' => self::BACULARIS_REPOSITORY_ADDR . '/stable/debian/ bullseye main',
 			'Debian 12 Bookworm' => self::BACULARIS_REPOSITORY_ADDR . '/stable/debian/ bookworm main',
+			'Debian 13 Trixie' => self::BACULARIS_REPOSITORY_ADDR . '/stable/debian/ trixie main',
 			'Ubuntu 20.04 Focal' => self::BACULARIS_REPOSITORY_ADDR . '/stable/ubuntu/ focal main',
 			'Ubuntu 22.04 Jammy' => self::BACULARIS_REPOSITORY_ADDR . '/stable/ubuntu/ jammy main',
 			'Ubuntu 24.04 Noble' => self::BACULARIS_REPOSITORY_ADDR . '/stable/ubuntu/ noble main',
